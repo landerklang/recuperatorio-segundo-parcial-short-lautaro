d03cf40be739c83914bcf5c62903169928771daa
¨lautaro <shortlauty35@gmail.com>
2025-07-10 14:40:59 -0300

    se a establecido correctamente la base de datos

---
+++ app.js
@@ -0,0 +1,19 @@
+import { sequelize } from "./src/config/database.js";
+import { staON } from "./src/config/database.js";
+import express from "express";
+import morgan from "morgan";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app= express();
+const PORT= process.env.PORT || 3000
+
+app.use(express.json());
+app.use(morgan("dev"));
+sequelize.authenticate
+
+app.listen(PORT,async()=>{
+    await staON();
+    console.log(`se escuhca correctamente el puerto ${PORT}`)
+})
