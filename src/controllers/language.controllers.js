eb76baae00c23ed42f44d4a269dec70c6353924b
¨lautaro <shortlauty35@gmail.com>
2025-07-11 05:39:52 -0300

    se terminaron los controkadores para actualizar un lenguaje y eliminar un lenguaje

--- src/controllers/language.controllers.js
+++ src/controllers/language.controllers.js
@@ -5,6 +5,9 @@ export const createlanguage = async(req, res)=>{
     if(name===""){
         return res.status(500).json({message: "no se permiten campos vacios"});
     };
+    const nombretab = await ProgrammingLanguage.findOne({where:{name}})
+    if(nombretab) { return res.status(500).json({message:"ya existe un nombre igual en la base de datos"})
+    };
     if(paradigm===""){
         return res.status(500).json({message: "no se permiten paradigmas vacios"});
     };
@@ -33,11 +36,36 @@ export const getalllanguage = async (req, res)=>{
     };
 };
 export const getlanguageByid =async(req, res)=>{
-
+    try {
+        const language = await ProgrammingLanguage.findByPk(req.params.id)
+        if (language) res.json(language);
+        else res.status(404).json({message: "no se encontro el lenguaje de programacion"})
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
 };
-export const updatelanguage= async(req,res)=>{
 
+export const updatelanguage= async(req,res)=>{
+    try{
+const [update] = await ProgrammingLanguage.update(req.body,{
+where: {id: req.params.id}
+});
+if (update) {
+    const updatelanguages= await ProgrammingLanguage.findByPk(req.params.id);
+    res.json(updatelanguages);
+} else {
+    res.status(404).json({message: "no se encontro el lenguaje"})
+}}catch(err){
+    res.status(500).json({error: err.message});
+
+}
 };
 export const delestlanguage =async(req,res)=>{
-
+try {
+    const deleted = await ProgrammingLanguage.destroy({where: {id: req.params.id}});
+    if (deleted) res.json({message: "el lenguaje de programacion se elimino"});
+    else return res.status(404).json({message: "no se pudo encontrar el lenguaje"});
+} catch (error) {
+    res.status(500).json({error: error.message});
+}
 };
